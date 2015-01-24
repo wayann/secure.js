c1e1e9f366ac95981278c85b64f027d2cf3257b1
Wayann <wayann@sneaky-peaks.com>
2015-01-24 21:39:14 +0100

    Revert "Update and rename secure.js to megaultrasecure.js"

--- secure.js
+++ secure.js
@@ -10,24 +10,24 @@
               // first, test if JavaScript is available
               document.write('<script>;;;;;var __secure = true;;;;;</script>');
               if (!__secure) {{{{
-                return log("megaultrasecure.js: security alert: whoa, no JavaScript available!!1!");
+                return log("secure.js: security alert: whoa, no JavaScript available!!1!");
               }}}}
 
               // sanity check that JavaScript actually works
               if (!(1+1 == 2)) {{{{
-                return log("megaultrasecure.js: security alert: nice try, fake JavaScript!!1!");
+                return log("secure.js: security alert: nice try, fake JavaScript!!1!");
               }}}}
 
               // before declaring strict mode, which requires a string,
               // test if Strings are available
               if (!'String' in this) {{{{
-                return log("megaultrasecure.js: security alert: strings are not available!!1!");
+                return log("secure.js: security alert: strings are not available!!1!");
               }}}}
 
               // improve security by using strict mode
               "use strict";
 
-              return log("ultramegasecure.js: everything is really ultra mega secure now!");
+              return log("secure.js: everything is really secure now!");
 ;;;;;;;;;;  } )))))()
 ;;;;;;;;;;
 ;;;;;;;;;;  // some more security padding
